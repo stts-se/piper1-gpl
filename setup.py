ae43e68c3cb05d9f7697d1570d40066905ca1b3b
Michael Hansen <mike@rhasspy.org>
2025-07-09 14:32:27 -0500

    Explicit encoding

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     description="Fast and local neural text-to-speech engine",
     url="http://github.com/OHF-voice/piper1-gpl",
     license="Apache-2.0",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="The Home Assistant Authors",
     author_email="hello@home-assistant.io",
