5ea446484cc0ac7ddc8ef9550d354a7e571d1f18
Michael Hansen <mike@rhasspy.org>
2025-07-09 11:39:55 -0500

    Using espeakbridge

--- src/piper/phonemize_espeak.py
+++ src/piper/phonemize_espeak.py
@@ -5,15 +5,6 @@ import unicodedata
 from pathlib import Path
 from typing import Union
 
-from .const import (
-    AUDIO_OUTPUT_SYNCHRONOUS,
-    CLAUSE_TYPE_SENTENCE,
-    IPA_PHONEMES,
-    ClauseTerminator,
-    EspeakError,
-    espeakCHARS_AUTO,
-)
-
 _DIR = Path(__file__).parent
 ESPEAK_DATA_DIR = _DIR / "espeak-ng-data"
 
@@ -23,65 +14,36 @@ class EspeakPhonemizer:
 
     def __init__(self, espeak_data_dir: Union[str, Path] = ESPEAK_DATA_DIR) -> None:
         """Initialize phonemizer."""
-        from . import espeak_ng  # avoid circular import
+        from . import espeakbridge  # avoid circular import
 
-        result = espeak_ng.espeak_Initialize(
-            AUDIO_OUTPUT_SYNCHRONOUS,
-            0,  # buflength
-            str(espeak_data_dir),
-            0,  # options
-        )
-        if result < 0:
-            raise EspeakError("Error initializing espeak-ng", result)
+        espeakbridge.initialize()
 
     def phonemize(self, voice: str, text: str) -> list[list[str]]:
         """Text to phonemes grouped by sentence."""
-        from . import espeak_ng  # avoid circular import
+        from . import espeakbridge  # avoid circular import
 
-        result = espeak_ng.espeak_SetVoiceByName(voice)
-        if result < 0:
-            raise EspeakError(f"Error setting espeak-ng voice to {voice}", result)
+        espeakbridge.set_voice(voice)
 
         all_phonemes: list[list[str]] = []
         sentence_phonemes: list[str] = []
 
-        remaining_text = text
-        while remaining_text:
-            phonemes_str, remaining_text, terminator_code = (
-                espeak_ng.py_espeak_TextToPhonemesWithTerminator(
-                    remaining_text, espeakCHARS_AUTO, IPA_PHONEMES
-                )
-            )
-
+        clause_phonemes = espeakbridge.get_phonemes(text)
+        for phonemes_str, terminator_str, end_of_sentence in clause_phonemes:
             # Filter out (lang) switch (flags).
             # These surround words from languages other than the current voice.
             phonemes_str = re.sub(r"\([^)]+\)", "", phonemes_str)
+
+            # Keep punctuation even though it's not technically a phoneme
+            phonemes_str += terminator_str
+            if terminator_str in (",", ":", ";"):
+                # Not a sentence boundary
+                phonemes_str += " "
 
             # Decompose phonemes into UTF-8 codepoints.
             # This separates accent characters into separate "phonemes".
             sentence_phonemes.extend(list(unicodedata.normalize("NFD", phonemes_str)))
 
-            terminator = ClauseTerminator.from_espeak(terminator_code)
-
-            # TODO: Use config for punctuation
-            if terminator == ClauseTerminator.PERIOD:
-                sentence_phonemes.append(".")
-            elif terminator == ClauseTerminator.QUESTION:
-                sentence_phonemes.append("?")
-            elif terminator == ClauseTerminator.EXCLAMATION:
-                sentence_phonemes.append("!")
-            elif terminator == ClauseTerminator.COMMA:
-                sentence_phonemes.append(",")
-                sentence_phonemes.append(" ")
-            elif terminator == ClauseTerminator.COLON:
-                sentence_phonemes.append(":")
-                sentence_phonemes.append(" ")
-            elif terminator == ClauseTerminator.SEMICOLON:
-                sentence_phonemes.append(";")
-                sentence_phonemes.append(" ")
-
-            if (terminator_code & CLAUSE_TYPE_SENTENCE) == CLAUSE_TYPE_SENTENCE:
-                # End of sentence
+            if end_of_sentence:
                 all_phonemes.append(sentence_phonemes)
                 sentence_phonemes = []
 
